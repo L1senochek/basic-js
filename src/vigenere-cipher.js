349b470d17dd89706b270b4dcbd85ffceb9130b9
Tatyana <106532861+L1senochek@users.noreply.github.com>
2023-04-17 13:45:36 +0300

    feat: update files (Mon, Apr 17, 2023 13:45:45)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,78 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value = true) {
+
+    this.type = value;
+    this.alphabet = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
   }
-  decrypt() {
+  encrypt(message, key) {
     // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    if (!key || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let upperMessage = message.toUpperCase();
+    let upperKey = key.toUpperCase();
+
+    let resultMessage = ``;
+    let indexKey = 0;
+
+    for (let i = 0; i < upperMessage.length; i++) {
+      if (this.alphabet.includes(upperMessage[i])) {
+        resultMessage += this.alphabet[((this.alphabet.indexOf(upperMessage[i])) + (this.alphabet.indexOf(upperKey[indexKey % key.length]))) % 26];
+        indexKey += 1;
+      } else {
+        resultMessage += upperMessage[i];
+      }
+    }
+    if (this.type === false) {
+      return resultMessage.split(``).reverse().join(``);
+    } else {
+      return resultMessage;
+    }
+  }
+  decrypt(encryptedMessage, key) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let upperEncryptedMessage = encryptedMessage.toUpperCase();
+    let upperKey = key.toUpperCase();
+
+    let resultEncryptedMessage = ``;
+    let indexKey = 0;
+
+    for (let i = 0; i < upperEncryptedMessage.length; i++) {
+      if (this.alphabet.includes(upperEncryptedMessage[i])) {
+        let indexCypher = this.alphabet.indexOf(upperEncryptedMessage[i]);
+        let keyIndex = this.alphabet.indexOf(upperKey[indexKey % key.length]);
+        let res = indexCypher - keyIndex;
+        let indexDecipheredStr;
+        if (res >= 0) {
+          indexDecipheredStr = res % 26;
+        } else {
+          indexDecipheredStr = (res + 26) % 26;
+        }
+        resultEncryptedMessage += indexDecipheredStr;
+        indexKey += 1;
+      } else {
+        resultEncryptedMessage += upperEncryptedMessage[i];
+      }
+
+    }
+    if (this.type === false) {
+      return resultEncryptedMessage.split(``).reverse().join(``);
+    } else {
+      return resultEncryptedMessage;
+    }
   }
 }
 
